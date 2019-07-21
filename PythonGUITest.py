9bd6436cc6b0ce10e39e82b89b76af582f703da9
chowJY <425970927@qq.com>
2019-07-21 13:05:33 +0800

    have to commit

--- PythonGUITest.py
+++ PythonGUITest.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
  
-from Tkinter import *      
+from Tkinter import *
 import os
+
 
 path = os.getcwd()
 disk = path.split('\\')[0]
@@ -19,16 +20,21 @@ listB  = Listbox(root, selectmode = MULTIPLE)
 for item in fileList:                 # 第一个小部件插入数据
     listB.insert(0,item)
 fileList.reverse()
-listB.pack()                    # 将小部件放置到主窗口中
-
+listB.pack()  
+                  # 将小部件放置到主窗口中
+import ctypes 
 def Call_Entry():
 	commitment = text.get(1.0, END)
 	commitment = commitment.split('\n')[0]
 	selectList = []
 	for i in listB.curselection():
 		selectList.append(fileList[i])
-	if len(selectList) <= 0 or len(commitment) <= 1:
+	if len(selectList) <= 0:
+		ctypes.windll.user32.MessageBoxW(0, u'至少选择一个文件', u'提示', 0)
+		# root.tk.messagebox.showinfo('Alert','请输入评论')
 		return
+	# if len(commitment) <= 1:
+	# 	ctypes.windll.user32.MessageBoxW(0, u'需要提交更新日志', u'提示', 0)
 	print selectList
 	uploadString = ''
 	for fileName in selectList:
@@ -39,7 +45,7 @@ def Call_Entry():
 	print uploadString
 	changePath = "{disk}\ncd {path}\n".format(disk = disk, path = path)
 	os.system(changePath)
-	commandLine = "git add {uploadfile}".format(uploadfile = uploadString)
+	commandLine = "git add {uploadfile}\n".format(uploadfile = uploadString)
 	os.system(commandLine)
 	commandLine = "git commit -m\"{commitment}\"".format(commitment = commitment)
 	os.system(commandLine)
